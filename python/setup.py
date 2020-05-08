1a86a3c2b76c750e20672989b5718611bf294bc4
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2020-05-08 17:33:11 +0900

    Add setup.py

---
+++ python/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='VL53L0X_rasp_python',
+    version="0.0.1",
+    description="Python interface to the VL53L0X ST Library",
+    long_description="",
+    author='John Bryan Moore, Ryo Okazaki',
+    license='MIT',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ]
+)
